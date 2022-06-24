7adeb317fe75c1aac2235057ee31544f1f905028
jang-yujin <jang-yujin@localhost>
2022-06-24 11:22:40 +0900

    make mainframe

--- Round-2-GraphicEditor/src/com/example/yujin/MainFrame.java
+++ Round-2-GraphicEditor/src/com/example/yujin/MainFrame.java
@@ -1,10 +1,37 @@
 package com.example.yujin;
+import java.awt.*;
+import javax.swing.*;
 
-public class MainFrame {
+import com.example.yujin.Menu;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("123");
+import java.awt.event.*;
+import java.util.*;
+
+public class MainFrame{
+	public static JFrame frame = new JFrame();
+	Menu menu = new Menu();
+
+	public MainFrame() {
+		JPanel panel = new JPanel();
+		JLabel label = new JLabel();
+
+		frame.setTitle("Graphic Editor");
+		frame.setSize(800,800);
+		frame.setResizable(false);
+		
+		
+		panel.add(new JLabel("출력 확인용 label"));
+		frame.add(panel);
+		frame.setVisible(true);
 	}
-
+	
+	public static void main(String[] args) {
+		new MainFrame();
+		//Screen screen = new Screen();
+		//Button button = new Button();
+		//screen.setScreen();
+		//screen.setScreen();
+		//menu.setMenu();
+		System.out.println("Main 출력 확인 ");
+	}
 }
